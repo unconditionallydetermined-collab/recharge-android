941997c30c0a8ce4562618172341c958e3b01047
CEM M <GEM@CEMs-Mac-mini.local>
2026-09-14 02:52:48 +0530

    Bump version to 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.recharge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
     }
 
